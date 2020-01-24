dddeed12adbc1449d5c41dcb748ba8dd449a211d
Billy Huang <10646003@ntub.edu.tw>
2020-01-24 12:33:30 +0800

    stock-market - stock/stock-item init

--- stock-market/src/app/app.module.ts
+++ stock-market/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
 
 // NgModule 這個 TypeScript 標記指出此類別定義為一個 Angular 模組
 @NgModule({
   // 宣告應用程式中使用的 HTML 元件與指示
   declarations: [
-    AppComponent
+    AppComponent,
+    StockItemComponent
   ],
   // 匯入其他 Angular 應用程式與函式庫模組
   imports: [
