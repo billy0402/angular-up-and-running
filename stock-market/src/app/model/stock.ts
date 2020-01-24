102cd201a4a08c68dad4724111b878186bb77354
Billy Huang <10646003@ntub.edu.tw>
2020-01-24 12:34:27 +0800

    stock-market - 使用模型做出更清楚的程式碼

--- stock-market/src/app/model/stock.ts
+++ stock-market/src/app/model/stock.ts
@@ -1,2 +1,16 @@
 export class Stock {
+
+  // 定義從 HTML 存取的各種欄位
+  favorite: boolean = false;
+
+  constructor(public name: string,
+              public code: string,
+              public price: number,
+              public previousPrice: number) {
+  }
+
+  isPositiveChange(): boolean {
+    return this.price >= this.previousPrice
+  }
+
 }
--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Stock} from '../../model/stock'
+
 @Component({
   // 使用此元件的選擇器。注意它前綴的 app，這是 Angular CLI 依預設加入的，可以自行指定。
   selector: 'app-stock-item',
@@ -9,30 +11,19 @@ import { Component, OnInit } from '@angular/core';
 // 實作 OnInit 介面，讓元件在初始化時取得掛鉤
 export class StockItemComponent implements OnInit {
 
-  // 定義從 HTML 存取的各種欄位
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public positiveChange: boolean;
-  public favorite: boolean;
+  public stock: Stock;
 
   constructor() { }
 
   // OnInit 函式在元件初始化時觸發
   ngOnInit() {
     // 初始化每個欄位的值
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 80;
-    this.positiveChange = this.price >= this.previousPrice;
-    this.favorite = false;
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
   }
 
   toggleFavorite(event) {
     console.log('We are toggling the favorite state for this stock', event);
-    this.favorite = !this.favorite;
+    this.stock.favorite = !this.stock.favorite;
   }
 
 }
