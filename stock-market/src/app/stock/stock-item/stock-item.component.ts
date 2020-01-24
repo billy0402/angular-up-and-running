4136e3d101b6402cfee59ab8d4108db0bb4cea05
Billy Huang <10646003@ntub.edu.tw>
2020-01-24 14:30:50 +0800

    stock-market - 內建屬性指令

--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -12,6 +12,8 @@ import {Stock} from '../../model/stock'
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses;
+  public stockStyles;
 
   constructor() { }
 
@@ -19,6 +21,18 @@ export class StockItemComponent implements OnInit {
   ngOnInit() {
     // 初始化每個欄位的值
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      'positive': this.stock.isPositiveChange(),
+      'negative': !this.stock.isPositiveChange(),
+      'large-change': largeChange,
+      'small-change': !largeChange
+    };
+    this.stockStyles = {
+      'color': this.stock.isPositiveChange() ? 'green' : 'red',
+      'font-size': largeChange ? '1.2em' : '0.8em'
+    };
   }
 
   toggleFavorite(event) {
