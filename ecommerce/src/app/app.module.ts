5917037e2616a84cc0b77b57f0f603493dac1e0b
Billy Huang <10646003@ntub.edu.tw>
2020-01-26 14:23:16 +0800

    ecommerce - products/product-create init

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductItemComponent } from './products/product-item/product-item.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductItemComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule
