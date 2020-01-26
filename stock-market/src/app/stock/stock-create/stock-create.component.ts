0beca4f54a0fd9f629bd70dab8fa304e9b57d0ff
Billy Huang <10646003@ntub.edu.tw>
2020-01-26 17:16:16 +0800

    stock-market - FormBuilder

--- stock-market/src/app/stock/stock-create/stock-create.component.ts
+++ stock-market/src/app/stock/stock-create/stock-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-stock-create',
@@ -8,13 +8,22 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class StockCreateComponent {
 
-  public stockForm: FormGroup = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0)])
-  });
+  // 宣告時不再初始化 FormGroup
+  public stockForm: FormGroup;
 
-  constructor() {
+  // 將 FormBuilder 實例注入建構元
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    // 使用注入的 FormBuilder 建構 FormGroup
+    this.stockForm = this.formBuilder.group({
+      // 以空值初始化名稱控制項與必要的檢驗程序
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    })
   }
 
   onSubmit() {
