2f333df2f17ecd39f63c2133731d1f9f9de11117
Billy Huang <10646003@ntub.edu.tw>
2020-01-30 03:30:54 +0800

    stock-market - 非同步單元測試
    RxJS 與可觀察: 非同步操作

--- stock-market/src/app/stock/stock-create/stock-create.component.spec.ts
+++ stock-market/src/app/stock/stock-create/stock-create.component.spec.ts
@@ -1,6 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, flush, TestBed, tick} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
 
-import { StockCreateComponent } from './stock-create.component';
+import {StockCreateComponent} from './stock-create.component';
+import {StockService} from '../../services/stock.service';
 
 describe('StockCreateComponent', () => {
   let component: StockCreateComponent;
@@ -8,9 +11,12 @@ describe('StockCreateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StockCreateComponent ]
+      declarations: [StockCreateComponent],
+      providers: [StockService],
+      // StockCreateComponent 需要 ReactiveFormsModule
+      imports: [ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +25,47 @@ describe('StockCreateComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  // 以 async 的回傳值作為 it 函式的第二個參數
+  it('should create stock through service', async(() => {
     expect(component).toBeTruthy();
-  });
+    component.stockForm.setValue({
+      name: 'My New Test Stock',
+      code: 'MNTS',
+      price: 100,
+      exchange: 'NYSE',
+      notablePeople: []
+    });
+    component.onSubmit();
+
+    // 等待測試執行非同步流程
+    fixture.whenStable().then(() => {
+      // 改變後更新視圖
+      fixture.detectChanges();
+      expect(component.messageService.message).toEqual('Stock with code MNTS successfully created.');
+      const messageEl = fixture.debugElement.query(By.css('.message')).nativeElement;
+      expect(messageEl.textContent).toBe('Stock with code MNTS successfully created.');
+    });
+  }));
+
+  // 使用 fakeAsync 取代 async
+  it('should create stock through service with fake async', fakeAsync(() => {
+    expect(component).toBeTruthy();
+    component.stockForm.setValue({
+      name: 'My New Test Stock',
+      code: 'MNTS',
+      price: 100,
+      exchange: 'NYSE',
+      notablePeople: []
+    });
+    component.onSubmit();
+
+    // 使用 tick() 模擬非同步行為
+    tick(1000); // 模擬時間
+    // flush(1); // 模擬次數
+    // 改變後更新視圖
+    fixture.detectChanges();
+    expect(component.messageService.message).toEqual('Stock with code MNTS successfully created.');
+    const messageEl = fixture.debugElement.query(By.css('.message')).nativeElement;
+    expect(messageEl.textContent).toBe('Stock with code MNTS successfully created.');
+  }));
 });
