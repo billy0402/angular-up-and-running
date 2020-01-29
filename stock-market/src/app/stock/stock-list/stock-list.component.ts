d0e1db9d0c3aa394f78d297fd5ec80cb1feba4a8
Billy Huang <10646003@ntub.edu.tw>
2020-01-29 18:20:22 +0800

    stock-market - 進階 HTTP 選項

--- stock-market/src/app/stock/stock-list/stock-list.component.ts
+++ stock-market/src/app/stock/stock-list/stock-list.component.ts
@@ -21,6 +21,26 @@ export class StockListComponent implements OnInit {
   ngOnInit() {
     // 使用 StockService 取得股票清單，呼叫並儲存可觀察
     this.stocks$ = this.stockService.getStocks();
+
+    this.stockService.getStocksAsResponse()
+      .subscribe((response) => {
+        console.log('OBSERVE "response" is ', response);
+      });
+
+    this.stockService.getStocksAsEvents()
+      .subscribe((response) => {
+        console.log('OBSERVE "events" is ', response);
+      });
+
+    this.stockService.getStocksAsString()
+      .subscribe((response) => {
+        console.log('OBSERVE Type "text" is ', response);
+      });
+
+    this.stockService.getStocksAsBlob()
+      .subscribe((response) => {
+        console.log('OBSERVE Type "blob" is ', response);
+      });
   }
 
 }
