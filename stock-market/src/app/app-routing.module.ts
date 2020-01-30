b57198ac439ef97e0ff2faf47d32a19c4829a51e
Billy Huang <10646003@ntub.edu.tw>
2020-01-30 22:12:30 +0800

    stock-market - 設定 Angular 導向

--- stock-market/src/app/app-routing.module.ts
+++ stock-market/src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
+import {LoginComponent} from './user/login/login.component';
+import {RegisterComponent} from './user/register/register.component';
+import {StockListComponent} from './stock/stock-list/stock-list.component';
+import {StockCreateComponent} from './stock/stock-create/stock-create.component';
 
+// 宣告應用程式的路徑陣列
+const appRoutes = [
+  // 加入預設路徑以重新導向到 Login 頁
+  // pathMatch 檢查 URL 與 full 是否完全相符
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'stock/list', component: StockListComponent},
+  {path: 'stock/create', component: StockCreateComponent},
+  // 加入捕捉全部不相符路徑重新導向到 Register 頁
+  // pathMatch 預設為 prefix，檢查 URL 開頭與 path 是否相符
+  {path: '**', redirectTo: '/register'}
+];
 
 @NgModule({
   declarations: [],
+  // 匯入並登記根應用程式的路徑
   imports: [
-    CommonModule
+    RouterModule.forRoot(appRoutes)
+  ],
+  // 匯出 RouterModule 使匯入 AppRoutingModule 的模組可存取路由指令
+  exports: [
+    RouterModule
   ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
