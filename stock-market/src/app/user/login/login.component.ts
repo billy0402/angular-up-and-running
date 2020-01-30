55176715542797d4705629324facaf2ff8a43b36
Billy Huang <10646003@ntub.edu.tw>
2020-01-30 22:16:51 +0800

    stock-market - 瀏覽應用程式

--- stock-market/src/app/user/login/login.component.ts
+++ stock-market/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 import {UserService} from '../../services/user.service';
 import {MessageService} from '../../services/message.service';
@@ -14,8 +15,10 @@ export class LoginComponent implements OnInit {
   public username: string = '';
   public password: string = '';
 
+  // 將 Router 注入元件
   constructor(private userService: UserService,
-              public messageService: MessageService) {
+              public messageService: MessageService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -26,6 +29,8 @@ export class LoginComponent implements OnInit {
       .subscribe((res) => {
         console.log('Successfully logged in');
         this.messageService.message = res.msg;
+        // 使用 Router 導向某個路徑 (絕對路徑)
+        this.router.navigate(['stock', 'list']);
       }, (err) => {
         console.error('Error logging in', err);
         this.messageService.message = err.error.msg;
