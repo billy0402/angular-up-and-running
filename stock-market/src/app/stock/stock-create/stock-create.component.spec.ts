c2d12b70f6ff3b8f5bc38d8e7e5a50ba3bef7d77
Billy Huang <10646003@ntub.edu.tw>
2020-01-30 14:16:57 +0800

    stock-market - HTTP 的單元測試
    HttpClient

--- stock-market/src/app/stock/stock-create/stock-create.component.spec.ts
+++ stock-market/src/app/stock/stock-create/stock-create.component.spec.ts
@@ -1,29 +1,37 @@
-import {async, ComponentFixture, fakeAsync, flush, TestBed, tick} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, flush, inject, TestBed, tick} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import {By} from '@angular/platform-browser';
 
 import {StockCreateComponent} from './stock-create.component';
 import {StockService} from '../../services/stock.service';
 
-describe('StockCreateComponent', () => {
+describe('StockCreateComponent With Real Service', () => {
   let component: StockCreateComponent;
   let fixture: ComponentFixture<StockCreateComponent>;
+  let httpBackend: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [StockCreateComponent],
       providers: [StockService],
       // StockCreateComponent 需要 ReactiveFormsModule
-      imports: [ReactiveFormsModule]
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule
+      ]
     })
       .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    httpBackend = backend;
     fixture = TestBed.createComponent(StockCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   // 以 async 的回傳值作為 it 函式的第二個參數
   it('should create stock through service', async(() => {
@@ -36,6 +44,14 @@ describe('StockCreateComponent', () => {
       notablePeople: []
     });
     component.onSubmit();
+
+    httpBackend.expectOne({
+      url: '/api/stock',
+      method: 'POST'
+    }, 'Create Stock with Failure')
+      .flush({
+        msg: 'Stock with code MNTS successfully created.'
+      });
 
     // 等待測試執行非同步流程
     fixture.whenStable().then(() => {
@@ -59,6 +75,14 @@ describe('StockCreateComponent', () => {
     });
     component.onSubmit();
 
+    httpBackend.expectOne({
+      url: '/api/stock',
+      method: 'POST'
+    }, 'Create Stock with Failure')
+      .flush({
+        msg: 'Stock with code MNTS successfully created.'
+      });
+
     // 使用 tick() 模擬非同步行為
     tick(1000); // 模擬時間
     // flush(1); // 模擬次數
@@ -68,4 +92,41 @@ describe('StockCreateComponent', () => {
     const messageEl = fixture.debugElement.query(By.css('.message')).nativeElement;
     expect(messageEl.textContent).toBe('Stock with code MNTS successfully created.');
   }));
+
+  it('should make call to stock and handle failure', async(() => {
+    expect(component).toBeTruthy();
+    component.stockForm.setValue({
+      name: 'My New Test Stock',
+      code: 'MNTS',
+      price: 100,
+      exchange: 'NYSE',
+      notablePeople: []
+    });
+    component.onSubmit();
+
+    let formValue = component.stockForm.value;
+    let newStock = {...formValue, previousPrice: formValue.price};
+
+    // 預期測試過程中對 /api/stock 發出一個 POST 請求
+    let httpReq = httpBackend.expectOne({
+      url: '/api/stock',
+      method: 'POST'
+    }, 'Create Stock with Failure');
+    // 確保 POST 請求的內容與元件中建構的股票相同
+    expect(httpReq.request.body).toEqual(newStock);
+    // 定義 POST 請求的回應為 400 失敗
+    httpReq.flush({msg: 'Stock already exists.'}, {status: 400, statusText: 'Failed!'});
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const messageEl = fixture.debugElement.query(By.css('.message')).nativeElement;
+      // 檢查伺服器回應正確的被元件顯示
+      expect(messageEl.textContent).toEqual('Stock already exists.');
+    });
+  }));
+
+  afterEach(() => {
+    // 確保 POST 請求在測試中發生
+    httpBackend.verify();
+  });
 });
--- stock-market/src/app/stock/stock-list/stock-list.component.spec.ts
+++ stock-market/src/app/stock/stock-list/stock-list.component.spec.ts
@@ -1,4 +1,7 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import {By} from '@angular/platform-browser';
 
 import {StockListComponent} from './stock-list.component';
 import {StockItemComponent} from '../stock-item/stock-item.component';
@@ -8,98 +11,138 @@ import {Stock} from '../../model/stock';
 describe('StockListComponent With Real Service', () => {
   let component: StockListComponent;
   let fixture: ComponentFixture<StockListComponent>;
+  let httpBackend: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [StockListComponent, StockItemComponent],
-      providers: [StockService]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StockListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should load stocks from real service on init', () => {
-    expect(component).toBeTruthy();
-    // 確保元件中的股票從服務載入
-    expect(component.stocks.length).toEqual(3);
-  });
-});
-
-describe('StockListComponent With Mock Service', () => {
-  let component: StockListComponent;
-  let fixture: ComponentFixture<StockListComponent>;
-  let stockService: StockService;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [StockListComponent, StockItemComponent],
-      providers: [StockService]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StockListComponent);
-    component = fixture.componentInstance;
-    // 總是透過元件的注入程序取得 StockService 服務
-    stockService = fixture.debugElement.injector.get(StockService);
-    // 模擬 getStocks 呼叫並回傳寫死的值
-    let spy = spyOn(stockService, 'getStocks')
-      .and.returnValue([
-        new Stock('Mock Stock', 'MS', 800, 900, 'NYSE')
-      ]);
-    fixture.detectChanges();
-  });
-
-  it('should load stocks from mocked service on init', () => {
-    expect(component).toBeTruthy();
-    // 確保元件中的股票來自模擬呼叫
-    expect(component.stocks.length).toEqual(1);
-    expect(component.stocks[0].code).toEqual('MS');
-  });
-});
-
-describe('StockListComponent With Fake Service', () => {
-  let component: StockListComponent;
-  let fixture: ComponentFixture<StockListComponent>;
-
-  beforeEach(async(() => {
-    // 定義實作 getStocks() 方法的 stockServiceFake 物件
-    let stockServiceFake = {
-      getStocks: () => {
-        return [
-          new Stock('Fake Stock', 'FS', 800, 900, 'NYSE')
-        ];
-      }
-    };
-    TestBed.configureTestingModule({
-      declarations: [StockListComponent, StockItemComponent],
-      providers: [
-        {
-          provide: StockService,
-          // 指定 StockService 的實例
-          useValue: stockServiceFake
-        }
+      providers: [StockService],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
       ]
     })
       .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    httpBackend = backend;
     fixture = TestBed.createComponent(StockListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    // 設定 /api/stock 呼叫作為測試的一部分
+    httpBackend.expectOne({
+      url: '/api/stock',
+      method: 'GET'
+      //  定義呼叫 GET 時回傳的股票清單
+    }, 'Get list of stocks')
+      .flush([
+        {
+          name: 'Test Stock 1',
+          code: 'TS1',
+          price: 80,
+          previousPrice: 90,
+          exchange: 'NYSE'
+        },
+        {
+          name: 'Test Stock 2',
+          code: 'TS2',
+          price: 800,
+          previousPrice: 900,
+          exchange: 'NYSE'
+        }
+      ]);
+  }));
 
-  it('should load stocks from fake service on init', () => {
+  it('should load stocks from real service on init', async(() => {
     expect(component).toBeTruthy();
-    expect(component.stocks.length).toEqual(1);
-    // 確保元件中的股票來自假的呼叫
-    expect(component.stocks[0].code).toEqual('FS');
+    // 確保元件中的股票從服務載入
+    expect(component.stocks$).toBeTruthy();
+
+    // 等待 Angular 工作佇列清空然後繼續
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const stockItems = fixture.debugElement.queryAll(By.css('app-stock-item'));
+      expect(stockItems.length).toEqual(2);
+    });
+  }));
+
+  afterEach(() => {
+    // 檢驗 /api/stock 的 GET 呼叫確實是測試的一部分
+    httpBackend.verify();
   });
 });
+
+// describe('StockListComponent With Mock Service', () => {
+//   let component: StockListComponent;
+//   let fixture: ComponentFixture<StockListComponent>;
+//   let stockService: StockService;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [StockListComponent, StockItemComponent],
+//       providers: [StockService]
+//     })
+//       .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(StockListComponent);
+//     component = fixture.componentInstance;
+//     // 總是透過元件的注入程序取得 StockService 服務
+//     stockService = fixture.debugElement.injector.get(StockService);
+//     // 模擬 getStocks 呼叫並回傳寫死的值
+//     let spy = spyOn(stockService, 'getStocks')
+//       .and.returnValue([
+//         new Stock('Mock Stock', 'MS', 800, 900, 'NYSE')
+//       ]);
+//     fixture.detectChanges();
+//   });
+//
+//   it('should load stocks from mocked service on init', () => {
+//     expect(component).toBeTruthy();
+//     // 確保元件中的股票來自模擬呼叫
+//     expect(component.stocks.length).toEqual(1);
+//     expect(component.stocks[0].code).toEqual('MS');
+//   });
+// });
+//
+// describe('StockListComponent With Fake Service', () => {
+//   let component: StockListComponent;
+//   let fixture: ComponentFixture<StockListComponent>;
+//
+//   beforeEach(async(() => {
+//     // 定義實作 getStocks() 方法的 stockServiceFake 物件
+//     let stockServiceFake = {
+//       getStocks: () => {
+//         return [
+//           new Stock('Fake Stock', 'FS', 800, 900, 'NYSE')
+//         ];
+//       }
+//     };
+//     TestBed.configureTestingModule({
+//       declarations: [StockListComponent, StockItemComponent],
+//       providers: [
+//         {
+//           provide: StockService,
+//           // 指定 StockService 的實例
+//           useValue: stockServiceFake
+//         }
+//       ]
+//     })
+//       .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(StockListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should load stocks from fake service on init', () => {
+//     expect(component).toBeTruthy();
+//     expect(component.stocks.length).toEqual(1);
+//     // 確保元件中的股票來自假的呼叫
+//     expect(component.stocks[0].code).toEqual('FS');
+//   });
+// });
