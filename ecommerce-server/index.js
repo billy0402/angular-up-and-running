1932d4a5e1baee06b71432b662c4058e41f80dc1
Billy Huang <10646003@ntub.edu.tw>
2020-01-31 01:10:45 +0800

    ecommerce-server - add user

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(bodyParser());
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use('/api/user', require('./user'));
+
 app.use('/api/product', require('./products'));
 
 app.listen(3000, () => console.log('App Server listening on port 3000!'));
